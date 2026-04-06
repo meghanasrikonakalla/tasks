003e6b30657fbb98636fda4fc80e46488876e7cd
Meghana Sri Konakalla <konakallameghana@gmail.com>
2026-04-06 17:30:08 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,111 @@
+#even or odd
+'''num=int(input("enter the number"))
+if num%2==0:
+    print("it is even")
+else:
+    print("it is odd")'''
+
+#voting
+'''age=int(input("enter the age"))
+if age>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")'''
+
+#leap year
+'''year=int(input("enter the year"))
+if year%4:
+    print("leap year")
+else:
+    print("not leap year")'''
+
+#guest code
+'''name=input("enter the name")
+if name=="Meghana":
+    print("Welcome Meghana")
+else:
+    print("welcome guest")'''
+
+#vowels
+'''a=["a","e","i","o","u"]
+b=input("enter the letter").lower()
+if b in a:
+    print("it is vowel")
+else:
+    print("it is consonant")'''
+    
+'''a=["Meghana","indu","maggi","ashika","vedha"]
+name=input("enter the name")
+if name in a:
+    print("Welcome",name)
+else:
+    print("welcome guest")'''
+
+# social media login
+#nested if
+'''username=input("enter the name")
+password=int(input("enter the password"))
+if username=="Meghana":
+    if password==1234:
+             print("login successful")
+else:
+    print("invalid credentials")'''
+
+'''username=input("enter the name")
+#password=int(input("enter the password"))
+if username=="Meghana":
+    if password==1234:
+             print("login successful")
+else:
+    print("invalid credentials")'''
+
+#if-esle
+'''username=input("enter the name")
+password=int(input("enter the password"))
+if username=="Meghana" and password==1234:
+    print("login successful")
+else:
+    print("invalid credentials")'''
+
+#bakery code if ,elif,else
+'''cake prize=1200
+redvelvet cake
+1000-chocolate cake
+800-cheese cake
+600-butterscotch'''
+             
+
+'''prize=int(input("enter the prize"))
+if prize == 1200:
+   print("redvelvet")
+elif prize == 1000:
+   print("chocolate")
+elif prize == 800:
+   print("cheese")
+elif prize == 600:
+   print("butterscotch")
+else:
+   print("sorry cake is not available")'''
+
+#pizza
+
+'''prize=int(input("enter the prize"))
+if prize==800:
+   print("crispy corn pizza")
+elif prize==600:
+   print("paneer pizza")
+elif prize==400:
+   print("corn pizza")
+elif prize==200:
+   print("frenchfries&coke")'''
+   
+
+
+
+
+
+
+    
+
+
+
